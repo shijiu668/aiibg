783a0cd29e85cc212cdd106da55c4706d3d3c4a4
shijiu <shijiu8861@gmail.com>
2025-05-20 15:43:17 +0800

    添加统计代码2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        
+
         {/* Google Analytics代码 - 页面加载后执行 */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-QQ7D0ST9FK"
@@ -45,6 +45,28 @@ export default function RootLayout({
             gtag('js', new Date());
             gtag('config', 'G-QQ7D0ST9FK');
           `}
+        </Script>
+
+        {/* Microsoft Clarity代码 - 页面加载后执行 */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rmemgiylp4");
+          `}
+
+          {/* Microsoft Clarity代码2 - 页面加载后执行 */}
+          <Script id="microsoft-clarity-2" strategy="afterInteractive">
+            {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rmbvtyddzo");
+          `}
+          </Script>
         </Script>
       </body>
     </html>
