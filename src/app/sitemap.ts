8fe9155a8360e629236e4ab938ab2dce3a697d8f
shijiu <shijiu8861@gmail.com>
2025-06-02 12:00:27 +0800

    更新站点地图

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,6 +15,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: 'https://italianbrainrots.org/pdf-to-brainrot',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
       url: 'https://italianbrainrots.org/italian-brainrot-clicker',
       lastModified: new Date(),
       changeFrequency: 'weekly',
